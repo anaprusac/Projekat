39da500c092b565799cf341bd26a9876e8735af1
anaprusac <anaprusac44@gmail.com>
2024-04-14 14:38:09 +0200

    Acquisitor

--- dCom-radna-verzija/dCom/ProcessingModule/Acquisitor.cs
+++ dCom-radna-verzija/dCom/ProcessingModule/Acquisitor.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ProcessingModule
@@ -57,6 +58,23 @@ namespace ProcessingModule
 		private void Acquisition_DoWork()
 		{
             //TO DO: IMPLEMENT
+            List<IConfigItem> config_items = configuration.GetConfigurationItems(); // pre akvizicije konfigurisemo sve signale
+
+            // akvizicija se radi u beskonacnoj petlji
+            while (true)
+            {
+                acquisitionTrigger.WaitOne();
+                foreach (IConfigItem item in config_items)
+                {
+                    item.SecondsPassedSinceLastPoll++;
+                    if (item.SecondsPassedSinceLastPoll == item.AcquisitionInterval)
+                    {
+                        processingManager.ExecuteReadCommand(item, this.configuration.GetTransactionId(),
+                            this.configuration.UnitAddress, item.StartAddress, item.NumberOfRegisters); // citanje akvizicionih podataka
+                        item.SecondsPassedSinceLastPoll = 0;
+                    }
+                }
+            }
         }
 
         #endregion Private Methods
